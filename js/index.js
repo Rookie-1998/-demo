4c3b5039f71fc3a6949cd2bd501041e4551ee539
Rookie-1998 <1013179321@qq.com>
2021-01-17 20:21:10 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    // 使用swiper插件
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+})
